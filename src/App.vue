df0a1d56cf796bc1919bab2ed4ad5726876a190b
Marcos Joél Sánchez López <mjsanchezl@mineco.gob.gt>
2022-12-07 11:26:41 -0600

    frontend vue completed

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="max-w-7xl mx-auto">
+      <nav>
+        <RouterLink to="/" class="px-4 py-2 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white">Home</RouterLink>
+        <RouterLink to="/skills" class="px-4 py-2 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white">Skills</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="max-w-7xl mx-auto min-h-screen">
+    <RouterView />
+  </div>
+</template>
+
---
+++ src/views/skills/SkillCreate.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { reactive } from 'vue';
+import useSkills from '../../composables/skills';
+
+const { storeSkill, errors } = useSkills();
+
+
+const form = reactive({
+  name:"",
+  slug:""
+})
+</script>
+
+<template>
+ <div class="mt-12">
+    <form action="" class="max-w-md mx-auto p-4 bg-white shadow-md rounded-md" @submit.prevent="storeSkill(form)">
+      <div class="space-y-6">
+        <div class="mb-6">
+          <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-indigo-500 text-xl">Name of Skill</label>
+          <input type="text" id="name" v-model="form.name"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <div v-if="errors.name">
+              <span class="text-sm text-red-600">{{ errors.name[0]}}</span>
+          </div>
+          <label for="slug" class="block mb-2 text-sm font-medium text-gray-900 dark:text-indigo-500 text-xl">Slug for Skill</label>
+          <input type="text" id="slug" v-model="form.slug"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <div v-if="errors.slug">
+              <span class="text-sm text-red-600">{{ errors.slug[0]}}</span>
+          </div>
+        </div>
+      </div>
+      <div class="mt-4">
+        <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded">
+          Save
+        </button>
+      </div>
+    </form>
+ </div>
+</template>
